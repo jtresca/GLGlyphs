0649a9d1e8f4ec9a71eca3be27bb992392de3c19
Rik Catlow <rcatlow@pcadmins-MacBook-Pro.local>
2013-12-02 11:46:10 -0600

    Added Pin that has a fill

--- ie7/ie7.js
+++ ie7/ie7.js
@@ -1,93 +1,96 @@
 /* To avoid CSS expressions while still supporting IE 7 and IE 6, use this script */
 /* The script tag referring to this file must be placed before the ending body tag. */
 
+/* Use conditional comments in order to target IE 7 and older:
+	<!--[if lt IE 8]><!-->
+	<script src="ie7/ie7.js"></script>
+	<!--<![endif]-->
+*/
+
 (function() {
 	function addIcon(el, entity) {
 		var html = el.innerHTML;
 		el.innerHTML = '<span style="font-family: \'GLGlyphs\'">' + entity + '</span>' + html;
 	}
 	var icons = {
-		'icon-project': '&#x61;',
-		'icon-contract2': '&#x68;',
-		'icon-follow': '&#x66;',
-		'icon-edit': '&#x67;',
-		'icon-watch': '&#xe603;',
-		'icon-email': '&#x69;',
-		'icon-multi_project': '&#x6a;',
-		'icon-lock': '&#x6b;',
-		'icon-phone': '&#x6c;',
-		'icon-list': '&#x6d;',
-		'icon-group': '&#x6e;',
-		'icon-x': '&#x6f;',
-		'icon-check': '&#x70;',
-		'icon-recruit': '&#x64;',
-		'icon-company': '&#x71;',
-		'icon-print': '&#x72;',
-		'icon-send_email': '&#x73;',
-		'icon-beaker': '&#x74;',
-		'icon-action_arrow': '&#x65;',
-		'icon-cog': '&#x75;',
-		'icon-money': '&#x76;',
-		'icon-contract': '&#x77;',
-		'icon-expand': '&#x78;',
-		'icon-pin': '&#x79;',
-		'icon-profile': '&#x7a;',
-		'icon-tiles': '&#x31;',
-		'icon-refresh': '&#x32;',
-		'icon-caution': '&#x33;',
-		'icon-idea': '&#x34;',
-		'icon-star': '&#x35;',
-		'icon-download': '&#x36;',
-		'icon-add2': '&#x37;',
-		'icon-new_burst': '&#x38;',
-		'icon-dashboard': '&#x30;',
-		'icon-grid': '&#x39;',
-		'icon-lines': '&#x3d;',
-		'icon-tick': '&#x60;',
-		'icon-alert': '&#x41;',
-		'icon-share': '&#x42;',
-		'icon-check2': '&#x43;',
-		'icon-heart': '&#x44;',
-		'icon-forward': '&#x45;',
-		'icon-back': '&#x46;',
-		'icon-car': '&#x47;',
-		'icon-search': '&#x48;',
-		'icon-arrow_down': '&#x49;',
-		'icon-arrow_up': '&#x4a;',
-		'icon-arrow_left': '&#x4b;',
-		'icon-arrow_right': '&#x4c;',
-		'icon-activity': '&#x4d;',
-		'icon-multi_doc': '&#x4e;',
-		'icon-add_doc': '&#x4f;',
-		'icon-stop': '&#x50;',
-		'icon-people': '&#x51;',
-		'icon-info': '&#x52;',
-		'icon-chevron_right': '&#x53;',
-		'icon-help': '&#x54;',
-		'icon-chevron_down': '&#x55;',
-		'icon-add': '&#x56;',
-		'icon-delete': '&#x57;',
-		'icon-trash': '&#x58;',
-		'icon-paper': '&#x59;',
-		'icon-thumbs_up': '&#x5a;',
-		'icon-tick-right': '&#x21;',
-		'icon-comment': '&#x63;',
-		'icon-time': '&#x62;',
+		'glg-project': '&#x61;',
+		'glg-contract2': '&#x68;',
+		'glg-follow': '&#x66;',
+		'glg-edit': '&#x67;',
+		'glg-watch': '&#xe603;',
+		'glg-email': '&#x69;',
+		'glg-multi_project': '&#x6a;',
+		'glg-lock': '&#x6b;',
+		'glg-phone': '&#x6c;',
+		'glg-list': '&#x6d;',
+		'glg-group': '&#x6e;',
+		'glg-x': '&#x6f;',
+		'glg-check': '&#x70;',
+		'glg-recruit': '&#x64;',
+		'glg-company': '&#x71;',
+		'glg-print': '&#x72;',
+		'glg-send_email': '&#x73;',
+		'glg-beaker': '&#x74;',
+		'glg-action_arrow': '&#x65;',
+		'glg-cog': '&#x75;',
+		'glg-money': '&#x76;',
+		'glg-contract': '&#x77;',
+		'glg-expand': '&#x78;',
+		'glg-pin': '&#x79;',
+		'glg-profile': '&#x7a;',
+		'glg-tiles': '&#x31;',
+		'glg-refresh': '&#x32;',
+		'glg-caution': '&#x33;',
+		'glg-idea': '&#x34;',
+		'glg-star': '&#x35;',
+		'glg-download': '&#x36;',
+		'glg-add2': '&#x37;',
+		'glg-new_burst': '&#x38;',
+		'glg-dashboard': '&#x30;',
+		'glg-grid': '&#x39;',
+		'glg-lines': '&#x3d;',
+		'glg-tick': '&#x60;',
+		'glg-alert': '&#x41;',
+		'glg-share': '&#x42;',
+		'glg-check2': '&#x43;',
+		'glg-heart': '&#x44;',
+		'glg-forward': '&#x45;',
+		'glg-back': '&#x46;',
+		'glg-car': '&#x47;',
+		'glg-search': '&#x48;',
+		'glg-arrow_down': '&#x49;',
+		'glg-arrow_up': '&#x4a;',
+		'glg-arrow_left': '&#x4b;',
+		'glg-arrow_right': '&#x4c;',
+		'glg-activity': '&#x4d;',
+		'glg-multi_doc': '&#x4e;',
+		'glg-add_doc': '&#x4f;',
+		'glg-stop': '&#x50;',
+		'glg-people': '&#x51;',
+		'glg-info': '&#x52;',
+		'glg-chevron_right': '&#x53;',
+		'glg-help': '&#x54;',
+		'glg-chevron_down': '&#x55;',
+		'glg-add': '&#x56;',
+		'glg-delete': '&#x57;',
+		'glg-trash': '&#x58;',
+		'glg-paper': '&#x59;',
+		'glg-thumbs_up': '&#x5a;',
+		'glg-tick-right': '&#x21;',
+		'glg-comment': '&#x63;',
+		'glg-time': '&#x62;',
+		'glg-pin_fill': '&#xe600;',
 		'0': 0
 		},
 		els = document.getElementsByTagName('*'),
-		i, attr, c, el;
+		i, c, el;
 	for (i = 0; ; i += 1) {
 		el = els[i];
 		if(!el) {
 			break;
 		}
-		attr = el.getAttribute('data-icon');
-		if (attr) {
-			addIcon(el, attr);
-		}
 		c = el.className;
-		c = c.match(/icon-[^\s'"]+/);
+		c = c.match(/glg-[^\s'"]+/);
 		if (c && icons[c[0]]) {
 			addIcon(el, icons[c[0]]);
 		}
